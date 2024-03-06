5c0203ab5f52b94dc0f0d70053bbd40a117879b6
Shivam Kumar Singh <128828636+shivamkumar74310@users.noreply.github.com>
2024-03-07 01:05:06 +0530

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,4 @@
+function orderItem(category, itemName, price) {
+    alert(`You have ordered a ${category.toLowerCase()} - ${itemName} for $${price.toFixed(2)}`);
+    // You can customize this function to perform other actions, e.g., updating a shopping cart.
+}
